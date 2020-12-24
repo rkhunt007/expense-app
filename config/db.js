4a8960bd9e33c53633d335fdd082477fbb5645e3
Rahul Khunt <rahul.khunt@techuplabs.com>
2020-12-24 23:19:15 +0530

    CORS

--- config/db.js
+++ config/db.js
@@ -2,8 +2,6 @@ const mongoose = require("mongoose");
 const config = require('config');
 const uri = process.env.mongoURI || config.get('mongoURI');
 
-console.log('process.env.mongoURI', process.env.mongoURI);
-
 const connectDB = async () => {
     try {
         await new mongoose.connect(uri, {
